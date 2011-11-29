1f5498d263300c999908f046efd260a3965f3ace
scotepi <scotepi@5fa7b2af-2ea6-40dc-ab77-c6c5b835fb0f>
2011-11-29 22:35:52 +0000

    Tagging as 0.4.1

---
+++ tags/0.4.1/locals.lua
@@ -0,0 +1,16 @@
+local addonName, L = ...;
+
+-- Default
+L["Alchemy"] = "Alchemy"
+L["Blacksmithing"] = "Blacksmithing"
+L["Enchanting"] = "Enchanting"
+L["Engineering"] = "Engineering"
+L["Herbalism"] = "Herbalism"
+L["Inscription"] = "Inscription"
+L["Jewelcrafting"] = "Jewelcrafting"
+L["Leatherworking"] = "Leatherworking"
+L["Mining"] = "Mining"
+L["No Profession"] = "No Profession"
+L["Skinning"] = "Skinning"
+L["Tailoring"] = "Tailoring"
+
